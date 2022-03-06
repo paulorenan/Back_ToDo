42b1d0be25180f921567356067e8eaec2dfecd96
Paulo Renan Almeida <able.paulorenan@gmail.com>
2022-03-06 18:50:17 -0300

    adiciona a rota get

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -4,11 +4,24 @@ import UserModel from '../models/UserModel';
 
 const UserRouter = Router();
 
-UserRouter.post('/', (req, res) => {
+UserRouter.post('/', async (req, res) => {
   try{
     const repo = getRepository(UserModel);
-    const result = repo.save(req.body);
+    const result = await repo.save(req.body);
     return res.status(201).json(result);
+  }
+  catch(err){
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+});
+
+UserRouter.get('/', async (req, res) => {
+  try{
+    const repo = getRepository(UserModel);
+    const result = await repo.find();
+    return res.status(200).json(result);
   }
   catch(err){
     return res.status(400).json({
