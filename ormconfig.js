41f323013b7ead29546dfbab57d6d7d84cb88afd
Paulo Renan Almeida <able.paulorenan@gmail.com>
2022-03-08 23:23:22 -0300

    teste34

--- ormconfig.js
+++ ormconfig.js
@@ -2,15 +2,16 @@ module.exports = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  url: process.env.DB_URL,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_USERNAME,
   logging: false,
   entities: [
-    "src/models/**/*.ts"
+    "build/models/**/*.ts"
   ],
   migrations: [
-    "src/database/migration/**/*.ts"
+    "build/database/migration/**/*.ts"
   ],
   subscribers: [
     "src/subscriber/**/*.ts"
